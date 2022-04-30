f65bd9f79848221fd84cd7f091b6a13180d80a93
zeyad taher <zeyadtaher16@gmail.com>
2022-04-30 19:12:23 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <stack>
+using namespace std;
+
+int F(int n)
+{
+    if(n<=1) return 1;
+    int a=n+F(n-1);
+    int b=n*F(n/2);
+    int c=n-2-(a+b)%2;
+    int d=F(c);
+    return a+b+d;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout<<F(2)<<endl;
     return 0;
 }
